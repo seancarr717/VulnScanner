3eb9caac01d1aeac5feaa2b1c35a2682ccf8c46c
seancarr717 <153228459+seancarr717@users.noreply.github.com>
2024-05-06 20:06:26 +0100

    fix prod and dev env fully and implement login error /success display

--- app/auth.py
+++ app/auth.py
@@ -13,13 +13,14 @@ def login():
             session['username'] = username
             return redirect(url_for('home'))
         else:
-            return 'Invalid username or password'
+            flash('Invalid username or password', 'error')  # Use flash to send a message
+            return redirect(url_for('login_route'))  # Redirect to login page again
     return render_template('login.html')
 
 def register(username, password):
     # Check if user already exists
     if User.query.filter_by(username=username).first():
-        flash('Username already exists.')
+        flash('Username already exists.', 'error')
         return redirect(url_for('register_route'))
 
     # Create new user with hashed password
@@ -28,7 +29,7 @@ def register(username, password):
     db.session.add(new_user)
     db.session.commit()
 
-    flash('User successfully registered!')
+    flash('User successfully registered!', 'success')
     return redirect(url_for('login_route'))
 
 def logout():
