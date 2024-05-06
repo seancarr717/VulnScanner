b8fccd8a8db77ffa9b0cd5803a7ed9879de485b4
seancarr717 <153228459+seancarr717@users.noreply.github.com>
2024-05-06 17:30:47 +0100

    dev and prod db fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
+import re
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Welcome181@localhost/postgres'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SECRET_KEY'] = 'melong'  
     
+    
+
+# Adjust the URI for SQLAlchemy 1.4+ compatibility
+    uri = os.getenv('DATABASE_URL', 'postgresql://postgres:Welcome181@localhost/postgres')
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+        app.config['SQLALCHEMY_DATABASE_URI'] = uri
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SECRET_KEY'] = 'melong'  
     
     db.init_app(app)
     
