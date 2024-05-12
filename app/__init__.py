c81e3124d161ebd8a0f166da17491f21cdb6f3bf
seancarr717 <153228459+seancarr717@users.noreply.github.com>
2024-05-12 22:55:31 +0100

    final push

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+
 import os
 db = SQLAlchemy()
-
-def create_app():
+#import db and create app then configure routes and serve app to wsgi
+def create_app(): 
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Welcome181@database-2.cdco2ayo4wwh.eu-north-1.rds.amazonaws.com:5432'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_secret_key')
     
     
     db.init_app(app)
     
-    migrate = Migrate(app, db)
 
     from .routes import configure_routes
     configure_routes(app)
